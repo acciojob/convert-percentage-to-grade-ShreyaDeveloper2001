f49e1e8de559d6a0e3505c1829930fdccc60b304
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-10-04 23:09:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function calculateGrade(percentage) {
   //your code here
+	if (isNaN(percentage) || percentage < 0) {
+    return 'F';
+  }
+
+  // Round the percentage to the nearest integer
+  const roundedPercentage = Math.round(percentage);
+
+  // Determine the letter grade based on the rounded percentage
+  if (roundedPercentage >= 90) {
+    return 'A';
+  } else if (roundedPercentage >= 80) {
+    return 'B';
+  } else if (roundedPercentage >= 70) {
+    return 'C';
+  } else if (roundedPercentage >= 60) {
+    return 'D';
+  } else {
+    return 'F';
+  }
 }
 const percentage = parseInt(prompt("Enter Percentage."));
 alert(calculateGrade(percentage));
